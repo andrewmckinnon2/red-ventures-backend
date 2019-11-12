2ab340dba393e4fba53f5677c5ea86c80d4c3347
Andrew McKinnon <andrewmckinnon2@gmail.com>
2019-11-11 22:01:45 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+
+var serverPort = 3000;
+var serverIpAddress = '127.0.0.1';
+
+app.listen(serverPort, serverIpAddress, function(){
+    console.log( "Listening on " + serverIpAddress + ", port " + serverPort )
+});
+
+module.exports = app;
